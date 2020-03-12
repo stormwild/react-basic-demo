9dbdbc8deb4d78bc98f4fddaca98527417f61a13
Alexander Torrijos <alexander.torrijos@crmdev.be>
2020-03-12 15:16:59 +0800

    Enabled JSX using Babel

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const element = React.createElement('div', { title: 'Outer div' },
-    React.createElement('h1', null, 'Hello World!')
+const element = (
+    <div title="Outer div">
+        <h1>Hello World!</h1>
+    </div>
 );
 
 ReactDOM.render(element, document.getElementById('content'));
