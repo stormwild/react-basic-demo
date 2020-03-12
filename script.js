342345dd7c86d655075089822c23712fb4810875
Alexander Torrijos <alexander.torrijos@crmdev.be>
2020-03-12 14:58:24 +0800

    Learning react basics

---
+++ script.js
@@ -0,0 +1,5 @@
+const element = React.createElement('div', { title: 'Outer div' },
+    React.createElement('h1', null, 'Hello World!')
+);
+
+ReactDOM.render(element, document.getElementById('content'));
